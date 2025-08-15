15bf0e8265c856bf39bc4192e43bac7a6e2e6468
Ali Samad <aliussamad2004@gmail.com>
2025-08-15 19:30:47 +0530

    Updated Reverse_string_using_stack

--- stack/reverse_string_in_stack.cpp
+++ stack/reverse_string_in_stack.cpp
@@ -1,18 +1,25 @@
 #include<iostream>
 #include<stack>
 using namespace std;
-int main(){
-    string str = "ALI";
+// we will be using stack to reverse a string
+string reverseString(string &str){
     stack<char> st;
     for(int i = 0; i < str.length(); i++){
         st.push(str[i]);
     }
-    string ans = " ";
+    string ans = "";
     while(!st.empty()){
         char ch = st.top();
         ans += ch;
         st.pop();
     }
-    cout << "The Reversed String is : " << ans << endl;
+    return ans;
+}
+int main(){
+    string str;
+    cout << "Enter a string to reverse: ";
+    getline(cin, str);
+    string result = reverseString(str);
+    cout << "Reversed String: " << result << endl;
     return 0;
 }
