5c986fa71d1e95cca8c68b30598b0172e7cfb04a
Ali Samad <aliussamad2004@gmail.com>
2025-08-21 01:08:39 +0530

    Arrays Questions

--- arrays/count0&1.cpp
+++ arrays/count0&1.cpp
@@ -1,20 +1,23 @@
-// Count 0s and 1s in an array
-#include <iostream>
+#include<iostream>
+#include <vector>
 using namespace std;
+int main(){
+    vector<int> arr = {0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0};
+    int size = 15;
+    int numZero = 0;
+    int numOne = 0;
+    for(int i = 0; i < size; i++){
+        if(arr[i] == 0){
+            numZero++;
+        } else if(arr[i] == 1){
+            numOne++;
+        } else {
+            cout << "Unexpected value in array at index: " << i << endl;
+        }
 
-int main() {
-    int arr[] = {0, 1, 0, 1, 0, 1, 1, 0};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    int count0 = 0, count1 = 0;
-    
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == 0) count0++;
-        else if (arr[i] == 1) count1++;
     }
-    
-    cout << "0s: " << count0 << endl;
-    cout << "1s: " << count1 << endl;
-    
+    cout << "The size of the array is: " << size << endl;
+    cout << "The total number of zeros in the array is: " << numZero << endl;
+    cout << "The total number of ones in the array is: " << numOne << endl;
     return 0;
 }
