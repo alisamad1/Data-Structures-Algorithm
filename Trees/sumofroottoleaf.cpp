13e79abaec169725013e79171edf6489d334d5be
Ali Samad <aliussamad2004@gmail.com>
2025-09-15 21:35:51 +0530

    Binary Tree Paths

---
+++ Trees/sumofroottoleaf.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+struct Node{
+    int data;
+    Node * left;
+    Node * right;
+    Node(int val): data(val), left(NULL), right(NULL){}
+};
+int solve(Node * root, int path){
+    if(root == NULL){
+        return 0;
+    }
+    path = path * 10 + root->data;
+    if(root->left == NULL && root->right == NULL){
+        return path;
+    }
+    return solve(root->left, path) + solve(root->right, path);
+}
+int sumNumbers(Node * root){
+    return solve(root, 0);
+}
+int main(){
+    Node * root = new Node(4);
+    root->left = new Node(9);
+    root->right = new Node(0);
+    root->left->left = new Node(5);
+    root->left->right = new Node(1);
+    int result = sumNumbers(root);
+    cout << "The Sum of the numbers from root to leaf node is : " << result << endl;
+    return 0;
+}
