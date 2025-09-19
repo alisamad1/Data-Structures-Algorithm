cedb89fdfa72095f1cc3203f91183a277b4696b8
Ali Samad <aliussamad2004@gmail.com>
2025-09-19 23:48:35 +0530

    Longest Common Prefix

--- Leetcode_Practice_Questions/LC_14.cpp
+++ Leetcode_Practice_Questions/LC_14.cpp
@@ -1,8 +1,13 @@
-string longestCommonPrefix(vector<string>& strs) {
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+string longestcommonPrefix(vector<string>& strs){
     if(strs.size() == 0){
-        return "";}
-    string  prefix = "";
-    for(int i = 0; i<strs[0].length(); i++){
+        return "";
+    }
+    string prefix = "";
+    for(int i = 0; i < strs[0].length(); i++){
         char ch = strs[0][i];
         for(string s : strs){
             if(ch == s[i]){
@@ -13,6 +18,12 @@ string longestCommonPrefix(vector<string>& strs) {
             }
         }
         prefix += ch;
-    }    
+    }
     return prefix;
+}
+int main(){
+    vector<string> strs = {"flower", "flow", "flight"};
+    string result = longestcommonPrefix(strs);
+    cout << "The result is : " << result << endl;
+    return 0;
 }
