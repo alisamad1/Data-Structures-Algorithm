18cf282890a073cd96fb6ad0ba2f8b414826b76e
Ali Samad <aliussamad2004@gmail.com>
2025-09-25 23:07:40 +0530

    Updated Code for Subsets

--- Leetcode_Practice_Questions/LC_78.cpp
+++ Leetcode_Practice_Questions/LC_78.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<vector>
-#include<cctype>
 using namespace std;
+void createSubset(vector<int>& nums, int index, vector<vector<int>>& res, vector<int>& subset);
 vector<vector<int>> subsets(vector<int>& nums){
-    vector<vector<int> res;
+    vector<vector<int>> res;
     vector<int> subset;
     createSubset(nums,0,res,subset);
     return res;
@@ -15,13 +15,26 @@ void createSubset(vector<int>& nums, int index, vector<vector<int>>& res, vector
     }
     subset.push_back(nums[index]);
     createSubset(nums,index+1,res,subset);
-    subset.pop_back();
+    subset.pop_back(); 
     createSubset(nums,index+1, res, subset);
 }
 int main(){
     int n;
-    cout<<"enter the numbers: ";
+    cout<<"Enter the number of elements: ";
     cin >> n;
     vector<int> nums(n);
-    '
+    cout << "Enter the elements: ";
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    vector<vector<int>> result = subsets(nums);
+    cout << "The subsets are: " << endl;
+    for(const auto& subset : result){
+        cout << "[ ";
+        for(int num : subset){
+            cout << num << " ";
+        }
+        cout << "]" << endl;
+    }
+    return 0;
 }
