0b99188654e2831b4ed584d75bebd6e2dd9d12be
Ali Samad <aliussamad2004@gmail.com>
2026-09-21 11:16:01 +0530

    Two Sum Updated

--- Leetcode_Practice_Questions/LC_1.cpp
+++ Leetcode_Practice_Questions/LC_1.cpp
@@ -1,13 +1,36 @@
-// we will be writing the code for two sum (Leetcode #1)
-vector<int> twoSum(vector<int>& nums, int target) {
-    int n = nums.size();
-    unordered_map<int,int> hashmap;
-    for(int i = 0; i<n;i++){
-        int diff = target - nums[i];
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+vector<int> twoSum(vector<int>& nums,int target, int n){
+    unordered_map<int, int> hashmap;
+    for(int i = 0; i < n; i++){
+        int diff = target -nums[i];
         if(hashmap.find(diff) != hashmap.end()){
             return {hashmap[diff],i};
         }
-        hashmap[nums[i]] = i;
+        hashmap[nums[i]] = i; 
     }
-    return{};
+    return {};
+}
+int main(){
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    int target;
+    cout  << "Enter the target value: ";
+    cin >> target;
+    vector<int> result = twoSum(nums, target, n);
+    if(!result.empty()){
+        cout << "Indices are:" << result[0] << " and " << result[1] << endl;
+    }
+    else{
+        cout << "No two sum solution found. " << endl;
+    }
+    return 0;
 }
--- Leetcode_Practice_Questions/LC_93.cpp
+++ Leetcode_Practice_Questions/LC_93.cpp
@@ -1,53 +1,39 @@
 #include<iostream>
 #include<vector>
 #include<string>
-#include<algorithm>
-using namespace std;
-bool valid(string s){
-    int n = s.size();
-    if(n == 1){
-        return true;
+using namespace std;                
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.length();
+        if (n == 0 || n > 3) return false;
+        if (n > 1 && s[0] == '0') return false;
+        int num = 0;
+        for (char c : s) {
+            if (!isdigit(c)) return false;
+            num = num * 10 + (c - '0');
+        }
+        return num <= 255;
     }
-    if(n > 3 || s[0] == '0'){
-        return false;
-    }
-    int val = stoi(s);
-    if(val > 255){
-        return false;
-    }
-    return true;
-}
-vector<string> restoreIpAddresses(string s){
-    int n = s.size();
-    vector<string> ans;
-    if(n > 12){
-        return ans;
-    }
-    for(int i = 1; i <= 3; i++){
-        for(int j = 1; j <= 3; j++){
-            for(int k = 1; k <= 3; k++){
-                if(i + j + k + 3 >= n && i + j + k < n){
-                    string a = s.substr(0,i);
-                    string b = s.substr(i,j);
-                    string c = s.substr(i + j, k);
-                    string d = s.substr(i + j + k);
-                    if(valid(a) && valid(b) && valid(c) && valid(d)){
-                        ans.push_back(a + "." + b + "." + c + "." + d);
-                    }
-                }
+    void backtrack(const string& s, int start, int segment, string currentIP, vector<string>& result) {
+        if (segment == 4 && start == s.length()) {
+            currentIP.pop_back(); // Remove the trailing dot
+            result.push_back(currentIP);
+            return;
+        }
+        if (segment >= 4) return;
+
+        for (int len = 1; len <= 3; ++len) {
+            if (start + len > s.length()) break;
+            string part = s.substr(start, len);
+            if (isValid(part)) {
+                backtrack(s, start + len, segment + 1, currentIP + part + ".", result);
             }
         }
     }
-    return ans;
-}
-int main(){
-    string s;
-    cout << "Enter the string: ";
-    cin >> s;
-    vector<string> result = restoreIpAddresses(s);
-    cout << "Possible valid IP addresses are: " << endl;
-    for(const string & it: result){
-        cout << it << endl;
-    }
-    return 0;
-}
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> result;
+        string currentIP;
+        backtrack(s, 0, 0, currentIP, result);
+        return result;
+    }   
